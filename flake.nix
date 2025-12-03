8e4de24b67949889bf8e3b3170d4c20c355b1153
Uzair Aftab <git@uzaaft.me>
2025-12-03 11:45:00 +0100

    nix: init nix flake with typescript-pnpm template

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import (inputs.nixpkgs) {inherit system;};
+      in {
+        devShell = pkgs.mkShell {
+          packages = [pkgs.vtsls];
+          buildInputs = [
+            pkgs.nodejs_22
+            pkgs.nodePackages.pnpm
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-go
+          ];
+        };
+      }
+    );
+}
