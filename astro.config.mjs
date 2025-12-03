c5d032591de183d2a3e5a9ddf24689f69a0e9126
Uzair Aftab <git@uzaaft.me>
2025-12-03 15:22:15 +0100

    feat: add cloudflare deps and wrangler.jsonc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    },
+    imageService: "cloudflare"
+  }),
   vite: {
     server: {
       allowedHosts: ['dev-2']
